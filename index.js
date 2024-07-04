bd2f12707686a47df2b502f74780673a68099bf9
esbatusadadda <juliakomorowicz16@gmail.com>
2024-07-04 18:29:59 +0700

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 import express from "express"
 import http from "http"
 dotenv.config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3006
 
 const app = express()
 app.use("/", express.static("public"))
